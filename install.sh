bc1a3ff149d06f7d74bf30cbf25a7d6771d153fb
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-02-13 10:32:57 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ 'command -v "brew" > /dev/null'
+if [ 'command -v "brew" > /dev/null' ]
 then
   read -p "Install tmux and neovim via brew? [y\n] " -n 1 -r
   echo ""
   if [[ ! $REPLY =~ ^[Yy]$ ]]
   then
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+    brew install tmux neovim
   fi
 elif [ 'command -v "pacman" > /dev/null' ]
 then
