eae47da71e75d1543970103f9f8f1c3600814e8f
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-02-23 21:30:00 -0600

    allow GNU cat and ls with gcat and gls respectively

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -4,12 +4,15 @@ if (( $+commands[eza] ))
 then
   alias ls="eza --color=always --color-scale=all --color-scale-mode=fixed --icons=always"
   alias tree="ls --tree"
+
+  alias gls="/dev/ls"
 fi
 alias ll="ls -alh"
 
 if (( $+commands[bat] ))
 then
   alias cat="bat"
+  alias gcat="/bin/cat"
 fi
 
 alias grep="grep --color=always"
