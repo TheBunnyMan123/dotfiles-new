5adc4b206a7bd2ebfc7b6653f1b2e4c0316cfbed
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-02-24 12:14:31 -0600

    assume arch because im not maintaining a script for 4 different package managers

--- install.sh
+++ install.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 
-PKGS="ripgrep tmux neovim zoxide fzf stow eza bat w3m"
+PKGS="ripgrep tmux neovim zoxide fzf stow eza bat w3m plasma-desktop sddm"
 
-if [ -x "$(command -v "brew")" ]
-then
-  read -p "Install [$PKGS] an midnight-commander via brew? [y\n] " -n 1 -r
-  echo ""
-  if [[ $REPLY =~ ^[Yy]$ ]]
-  then
-    brew install $PKGS midnight-commander
-  fi
-elif [ -x "$(command -v "pacman")" ]
-then
-  sudo pacman -S zsh $PKGS mc
-elif [ -x "$(command -v "apt")" ]
-  sudo apt install zsh $PKGS mc
-elif [ -x "$(command -v "dnf")" ]
-then
-  sudo dnf install zsh $PKGS mc
-else
-  echo "Please install $PKGS and midnight-commander"
-fi
+sudo pacman -S zsh $PKGS mc
 
 echo "Installing JetBrains Mono Nerd Font"
 TMPDIR="$(mktemp -d /tmp/nerd_fonts_XXXXXX)"
