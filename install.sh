4cd6c484cc4f584008412006680a3b4c347d3b39
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-02-27 17:39:42 -0600

    MORE XFCE STUFF

--- install.sh
+++ install.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 PKGS="flameshot xfce4 xfce4-goodies base-devel pipewire-pulse firefox-developer-edition tmux neovim zoxide fzf stow eza bat w3m plasma-desktop sddm"
-
+declare -a AURPKGS=("yay" "catppuccin-cursors-macchiato" "catppuccin-gtk-theme-macchiato" "caffeine-ng")
 sudo pacman -S zsh $PKGS mc --needed
 
 sudo systemctl enable sddm --force
 
-cd $(mktemp -d /tmp/catppuccin-gtk-aur)
-git clone https://aur.archlinux.org/catppuccin-gtk-theme-macchiato.git catppuccin-gtk
-cd catppuccin-gtk
-makepkg -si
-
-cd $(mktemp -d /tmp/caffeine-ng-aur)
-git clone https://aur.archlinux.org/caffeine-ng.git caffeine-ng
-cd caffeine-ng
-makepkg -si
+cd $(mktemp -d /tmp/aur-XXXXXX)
+for PKG in "${!AURPKGS[@]}"
+do
+  git clone "https://aur.archlinux.org/${AURPKGS[$PKG]}.git" ${AURPKGS[$PKG]}
+  cd ${AURPKGS[$PKG]}
+  makepkg -si
+  cd ..
+done
 
 firefox-developer-edition "https://github.com/catppuccin/dark-reader"
 firefox-developer-edition "https://color.firefox.com/?theme=XQAAAAJHBAAAAAAAAABBqYhm849SCicxcUcPX38oKRicm6da8pG5gi-DrbS7fiEFLUzDsWXWyUHMSkHZ2PpRK_LvZGTF44fp7VjbASbxkoZAmYAwEJIoRnjw8xrOTGV_TjmyI1jBzmpM9P7ysk1XcU5Vim_Fm-lEdd2D1sQPsP1UC1SXCENsCDFL_dvGtcmeCmbfO6OGGOJq3ruAS_Gz79X7J3IPhPKRi0y17vC4T81wBaZnK5IwST8XwXhsZW2xuHYXlQW9lWh_0MNERp0985NX-OWb_-ihrUcCUtuKvpNVRhWiOZEGlf8aqXacHiX1iC-UFzj9yYek5qPRUzkk0PbDL3_klrAKV3FhiFwzCdwhv8YLU-O8D_iavg6KYN8B5YNgd1CGF1betSDtAH2A8YHzjQlW16NmIECM0pNIUwPIAfdyRQ-aJb506nBvtvjIb3D_pn9BgwOl-Ok0CmqQ0Tzjt5DGy1bJL8OX8lKslypKhSobuDw63XSwbG9yxD4rKvo_NnhWJBdY1xtGLs4MOLeRe4Oc10HPwY2o-9WL3ktoaN2k17lOsMoF836BLjf_wIADwA"
@@ -27,7 +26,5 @@ unzip ./JetBrainsMono.zip -d ./patched-fonts/
 curl https://raw.githubusercontent.com/ryanoasis/nerd-fonts/96497b4fef014743867b6289f7761408f9e04e89/install.sh | bash
 
 chsh -s /bin/zsh
-
-stow .
 
 echo "Please run the xfce4.sh file after starting xfce4"
