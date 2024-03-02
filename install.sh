685e4c7e06ad031c808e3507012dfa979c10ffcd
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-03-02 01:55:52 +0000

    conform to arch guidelines as much as possible

--- install.sh
+++ install.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-PKGS="xorg-server flameshot xfce4 xfce4-goodies base-devel pipewire-pulse firefox-developer-edition tmux neovim zoxide fzf stow eza bat w3m plasma-desktop sddm"
-declare -a AURPKGS=("yay" "catppuccin-cursors-macchiato" "catppuccin-gtk-theme-macchiato" "caffeine-ng")
+PKGS="xdg-desktop-portal xdg-desktop-portal-gtk xorg-server flameshot xfce4 xfce4-goodies base-devel pipewire-pulse firefox-developer-edition tmux neovim zoxide fzf stow eza bat w3m alacritty keepassxc syncthing"
+AURPKGS="prismlauncher-bin catppuccin-cursors-macchiato catppuccin-gtk-theme-macchiato caffeine-ng"
 sudo pacman -S zsh $PKGS mc --needed
 
 sudo systemctl enable sddm --force
 
-cd $(mktemp -d /tmp/aur-XXXXXX)
-for PKG in "${!AURPKGS[@]}"
-do
-  git clone "https://aur.archlinux.org/${AURPKGS[$PKG]}.git" ${AURPKGS[$PKG]}
-  cd ${AURPKGS[$PKG]}
-  makepkg -si
-  cd ..
-done
+cd $(mktemp -d /tmp/yay)
+git clone "https://aur.archlinux.org/yay-bin.git" yay-bin
+cd yay-bin
+makepkg -si
+cd ..
+
+yay -S $AURPKGS
 
 firefox-developer-edition "https://github.com/catppuccin/dark-reader"
 firefox-developer-edition "https://color.firefox.com/?theme=XQAAAAJHBAAAAAAAAABBqYhm849SCicxcUcPX38oKRicm6da8pG5gi-DrbS7fiEFLUzDsWXWyUHMSkHZ2PpRK_LvZGTF44fp7VjbASbxkoZAmYAwEJIoRnjw8xrOTGV_TjmyI1jBzmpM9P7ysk1XcU5Vim_Fm-lEdd2D1sQPsP1UC1SXCENsCDFL_dvGtcmeCmbfO6OGGOJq3ruAS_Gz79X7J3IPhPKRi0y17vC4T81wBaZnK5IwST8XwXhsZW2xuHYXlQW9lWh_0MNERp0985NX-OWb_-ihrUcCUtuKvpNVRhWiOZEGlf8aqXacHiX1iC-UFzj9yYek5qPRUzkk0PbDL3_klrAKV3FhiFwzCdwhv8YLU-O8D_iavg6KYN8B5YNgd1CGF1betSDtAH2A8YHzjQlW16NmIECM0pNIUwPIAfdyRQ-aJb506nBvtvjIb3D_pn9BgwOl-Ok0CmqQ0Tzjt5DGy1bJL8OX8lKslypKhSobuDw63XSwbG9yxD4rKvo_NnhWJBdY1xtGLs4MOLeRe4Oc10HPwY2o-9WL3ktoaN2k17lOsMoF836BLjf_wIADwA"
