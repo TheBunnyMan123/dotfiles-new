d6a5f267b0ddf44028dc98d0e731fb395225af72
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-03-03 15:11:57 -0600

    neovim and xfce

--- .config/nvim/lua/thekillerbunny/lazyload.lua
+++ .config/nvim/lua/thekillerbunny/lazyload.lua
@@ -1,20 +1,20 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{
-		"michaelrommel/nvim-silicon",
-		lazy = true,
+  {
+    "michaelrommel/nvim-silicon",
+    lazy = true,
     cmd = "Silicon",
     init = function()
       local wk = require("which-key")
@@ -22,17 +22,17 @@ require("lazy").setup({
         ["<leader>sc"] = { ":Silicon<CR>", "Snapshot Code" },
       }, { mode = "v" })
     end,
-    config = function()
-      require("silicon").setup({
-        -- Configuration here, or leave empty to use defaults
-        font = "JetBrainsMono Nerd Font Mono=34;Noto Color Emoji=34"
-      })
+    opts = {
+      font = "JetBrainsMono Nerd Font Mono=34;Noto Color Emoji=34"
+    },
+    config = function(_, opts)
+      require("silicon").setup(opts)
     end,
-	},
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		dependencies = { 'nvim-lua/plenary.nvim' },
-		config = function()
+  },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
       local wk = require("which-key")
       wk.register({
         ["<leader>ff"] = { ":Telescope find_files<CR>", "Find Files" },
@@ -40,107 +40,109 @@ require("lazy").setup({
       wk.register({
         ["<leader>gf"] = { ":Telescope git_files<CR>", "Git Files" },
       }, { mode = "n" })
-		end
-	},
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x',
-	config = function()
-		local lsp_zero = require('lsp-zero')
-		lsp_zero.extend_lspconfig()
-
-		lsp_zero.on_attach(function(client, bufnr)
-			-- see :help lsp-zero-keybindings
-			-- to learn the available actions
-			lsp_zero.default_keymaps({buffer = bufnr})
-		end)
-
-		-- to learn how to use mason.nvim with lsp-zero
-		-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
-		require('mason').setup({})
-		require('mason-lspconfig').setup({
-			ensure_installed = {"lua_ls", "omnisharp"},
-			handlers = {
-				lsp_zero.default_setup,
-			},
-		})
-	end
-},
-{'williamboman/mason.nvim'},
-{'williamboman/mason-lspconfig.nvim'},
-{'neovim/nvim-lspconfig'},
-{'hrsh7th/cmp-nvim-lsp'},
-{'hrsh7th/nvim-cmp'},
-{'L3MON4D3/LuaSnip'},
-{ "catppuccin/nvim", name = "catppuccin", priority = 1000,
-config = function()
-	require("catppuccin").setup({
-		flavour = "macchiato", -- latte, frappe, macchiato, mocha
-		background = { -- :h background
-		light = "latte",
-		dark = "macchiato",
-	},
-	transparent_background = false, -- disables setting the background color.
-	show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
-	term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-	dim_inactive = {
-		enabled = false, -- dims the background color of inactive window
-		shade = "dark",
-		percentage = 0.15, -- percentage of the shade to apply to the inactive window
-	},
-	no_italic = false, -- Force no italic
-	no_bold = false, -- Force no bold
-	no_underline = false, -- Force no underline
-	styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-	comments = { "italic" }, -- Change the style of comments
-	conditionals = { "italic" },
-},
-color_overrides = {},
-custom_highlights = {},
-integrations = {
-	cmp = true,
-	gitsigns = true,
-	nvimtree = true,
-	treesitter = true,
-	notify = false,
-	mini = {
-		enabled = true,
-		indentscope_color = "",
-	},
-	-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-},
-})
-
--- setup must be called before loading
-vim.cmd.colorscheme "catppuccin"
-end
+    end
   },
   {
-	  "nvim-treesitter/nvim-treesitter",
-	  build = ":TSUpdate",
-	  config = function ()
-		  local configs = require("nvim-treesitter.configs")
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    config = function()
+      local lsp_zero = require('lsp-zero')
+      lsp_zero.extend_lspconfig()
 
-		  configs.setup({
-			  ensure_installed = { "c", "lua", "pkl", "json", "vim", "vimdoc", "query", "java", "c_sharp", "nix" },
-			  sync_install = true,
-			  highlight = { enable = true },
-			  indent = { enable = false },  
-		  })
-	  end
+      lsp_zero.on_attach(function(_, bufnr)
+        lsp_zero.default_keymaps({buffer = bufnr})
+      end)
+
+      require('mason').setup({})
+      require('mason-lspconfig').setup({
+        ensure_installed = {"lua_ls", "omnisharp"},
+        handlers = {
+          lsp_zero.default_setup,
+        },
+      })
+    end
+  },
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {'L3MON4D3/LuaSnip'},
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "macchiato", -- latte, frappe, macchiato, mocha
+      background = { -- :h background
+        light = "latte",
+        dark = "macchiato",
+      },
+      transparent_background = true, -- disables setting the background color.
+      show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
+      term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
+      no_italic = false, -- Force no italic
+      no_bold = false, -- Force no bold
+      no_underline = false, -- Force no underline
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+      },
+      color_overrides = {},
+      custom_highlights = {},
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+      },
+    },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+
+      -- setup must be called before loading
+      vim.cmd.colorscheme "catppuccin"
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        ensure_installed = { "c", "lua", "pkl", "json", "vim", "vimdoc", "query", "java", "c_sharp", "nix" },
+        sync_install = true,
+        highlight = { enable = true },
+        indent = { enable = false },
+      })
+    end
   },
   { "apple/pkl-neovim", build=":TSInstall pkl" },
   {
-	  "folke/which-key.nvim",
-	  event = "VeryLazy",
-	  init = function()
-		  vim.o.timeout = true
-		  vim.o.timeoutlen = 500
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 500
 
-	vim.g.mapleader = " "
+      vim.g.mapleader = " "
 
-local wk = require("which-key")
-wk.register({
-  ["<leader>tr"] = { ":Ex<CR>", "File Tree" },
-}, { mode = "n" })	
-	  end,
+      local wk = require("which-key")
+      wk.register({
+        ["<leader>tr"] = { ":Ex<CR>", "File Tree" },
+      }, { mode = "n" })
+    end,
   }
 })
